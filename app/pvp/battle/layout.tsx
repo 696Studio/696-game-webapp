9721f299f3cdea3860090d7ea84b93bb6df1faf7
696Studio <kostyan.kudin44@gmail.com>
2026-01-02 05:20:56 +0200

    PVP: STEP A test death FX (css + layout + debug trigger)

---
+++ app/pvp/battle/layout.tsx
@@ -0,0 +1,6 @@
+import type { ReactNode } from "react";
+import "./battle.animations.css";
+
+export default function BattleLayout({ children }: { children: ReactNode }) {
+  return <>{children}</>;
+}
